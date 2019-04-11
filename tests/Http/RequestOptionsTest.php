b1910588b55b99831a8ea6601a464c8ddef07fd6
frets1700 <panmol1700@gmail.com>
2019-04-11 17:06:42 -0400

    minor fix to patch method , probably need to check and pass it as a DI instance, adding some more tests

---
+++ tests/Http/RequestOptionsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Symplicity\Outlook\Tests\Http;
+
+use Symplicity\Outlook\Http\RequestOptions;
+use Symplicity\Outlook\Utilities\RequestType;
+
+class RequestOptionsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHeaders()
+    {
+        $requestOptions = new RequestOptions('api/outlook.php', new RequestType(RequestType::Get), [
+            'headers' => ['foo' => 'bar'],
+            'queryParams' => [1 => 2],
+            'token' => 'abc'
+        ]);
+
+        $requestOptions->addDefaultHeaders();
+        $headers = $requestOptions->getHeaders();
+        $this->assertArrayHasKey('foo', $headers);
+        $this->assertArrayHasKey('client-request-id', $headers);
+        $this->assertArrayHasKey('Accept', $headers);
+        $this->assertArrayHasKey('Authorization', $headers);
+        $this->assertEquals('Bearer abc', $headers['Authorization']);
+        $clientId = $headers['client-request-id'];
+
+        $requestOptions->resetUUID();
+        $this->assertNotEquals($clientId, $requestOptions->getHeaders()['client-request-id']);
+
+        $requestOptions = new RequestOptions('api/outlook.php', new RequestType(RequestType::Get), [
+            'headers' => ['foo' => 'bar'],
+            'queryParams' => [1 => 2]
+        ]);
+
+        $this->expectExceptionMessage('Missing Token');
+        $requestOptions->addDefaultHeaders();
+    }
+}
