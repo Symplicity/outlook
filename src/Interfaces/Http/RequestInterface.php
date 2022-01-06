bb0ba6d1af5726947f77b7261e55fdb165299e02
Arthur Sávio Figueiredo de Souza <arsafiso@gmail.com>
2022-01-06 20:28:51 -0300

    Retry connection with a new header

--- src/Interfaces/Http/RequestInterface.php
+++ src/Interfaces/Http/RequestInterface.php
@@ -6,4 +6,11 @@ namespace Symplicity\Outlook\Interfaces\Http;
 
 interface RequestInterface
 {
+    /**
+     * Get Headers with refreshed token
+     * @param string $url
+     * @param array $params
+     * @return array
+     */
+    public function getHeadersWithToken(string $url, array $params = []) : array;
 }
